c60e5098143fda5cdf41d0d1c3eec04044a041a7
Alvaro Floriano <a.floriano@cesjuanpablosegundocadiz.es>
2026-02-10 18:29:31 +0100

    Add status to Task structure

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ typedef char TaskDescription[300];
 typedef struct {
     TaskName name;
     TaskDescription description;
+    int stauts;
 } Task;
 
 
@@ -16,6 +17,7 @@ int main() {
     Task newTask;
     strcpy(newTask.name, "Tender");
     strcpy(newTask.description, "Tengo que tender la ropa antes de que llueva");
+    newTask.stauts = -1;
 
     printf("TASK: %s\n", newTask.name);
     printf("DESCRIPCION: %s\n", newTask.description);
